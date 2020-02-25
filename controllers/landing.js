096d5f32d385dfcdefeebc550fb65498915b4d06
Chance Onyiorah <chanceonyiorah@Chances-MacBook-Pro.local>
2020-02-24 21:14:37 -0500

    Svae leads collected on landing page

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,12 +1,14 @@
 
+const models = require('../models')
 
-exports.get_landing = function(req, res, next)
-{
+exports.get_landing = function(req, res, next){
   res.render('landing', { title: 'Express' });
 }
 
-exports.submit_lead = function(req, res, next)
-{
-  console.log("lead_email:", req.body.lead_email);
-  res.redirect('/');
+exports.submit_lead = function(req, res, next){
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect('/');
+  })
 }
